4a10e62a7aae6c60a50bdd87a6be2466128c86d1
unknown <valentinbrizard@hotmail.fr>
2023-02-15 14:27:58 +0100

    navbarfini

--- js/ModeJN.js
+++ js/ModeJN.js
@@ -1,10 +1,10 @@
 if (localStorage.getItem("mode") == "night") {
     document.body.style.color = "rgb(255, 255, 255)";
-        document.body.style.backgroundColor = "rgb(0, 0, 0)";
+        document.body.style.backgroundColor = "rgb(48, 16, 16)";
         modeNuit = false;
 
     } else {
-        document.body.style.color = "rgb(0, 0, 0)";
+        document.body.style.color = "rgb(48, 16, 16)";
         document.body.style.backgroundColor = "rgb(255, 255, 255)";
         modeNuit = true;
 }
